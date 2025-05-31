83750447e95de14556eeab3e767b57957e3c1ba7
PedroL <pedrophlima04@gmail.com>
2025-05-30 22:18:18 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppEventos/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiAppEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.NewPage1());
+
+        }//fim App
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 700;
+
+            return window;
+        }
+
+    }//fim classe
+}//fim namespace
